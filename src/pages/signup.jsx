82460f870eb13f464e16e37501c6b6cca31afea9
Ajaybanu <ajaybanu7@gmail.com>
2023-09-19 22:49:55 +0530

    m

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -3,44 +3,45 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
 import axios from "axios";
 import server from "../server"
-import { toast } from "react-toastify";
-
+import {useNavigate} from "react-router-dom"
 
 
 const Signup = () => {
 
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [number,setNumber]=useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState(null);
+  const navigate = useNavigate();
 
   const handleFileInputChange = (e) => {
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setAvatar(reader.result);
-      }
-    };
-
-    reader.readAsDataURL(e.target.files[0]);
+    const file = e.target.files[0];
+    setAvatar(file);
   };
 
   const handleSubmit = async (e) => {
+  
     e.preventDefault();
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+    const newForm = new FormData();
+
+    newForm.append("file", avatar);
+    newForm.append("name", name);
+    newForm.append("number", number );
+    newForm.append("email",email);
+    newForm.append("password",password)
 
     axios
-      .post(`${server}/user/create-user`, { name, email, password, avatar })
+      .post(`${server}/user/create-user`,newForm, config)
       .then((res) => {
-        toast.success(res.data.message);
-        setName("");
-        setEmail("");
-        setPassword("");
-        setAvatar();
+        if(res.data.success === true){
+          navigate("/login");
+        }
       })
       .catch((error) => {
-        toast.error(error.response.data.message);
+       console.log(error);
       });
   };
 
@@ -74,6 +75,29 @@ const Signup = () => {
               />
             </div>
           </div>
+
+
+
+          <div>
+            <label
+              htmlFor="number"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Contact.NO
+            </label>
+            <div className="mt-1">
+              <input
+                type="number"
+                name="number"
+                autoComplete="number"
+                required
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
 
           <div>
             <label
@@ -170,13 +194,14 @@ const Signup = () => {
               Submit
             </button>
           </div>
+          </form>
           {/* <div className={`${styles.noramlFlex} w-full`}>
             <h4>Already have an account?</h4>
             <Link to="/login" className="text-blue-600 pl-2">
               Sign In
             </Link>
           </div> */}
-        </form>
+
       </div>
     </div>
   </div>
